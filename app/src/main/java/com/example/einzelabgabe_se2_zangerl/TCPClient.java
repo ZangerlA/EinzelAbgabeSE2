0d042ef3b9229c8d4725a7e20df97390c04dadf6
ZangerlA <azangerl@gmail.com>
2021-03-11 12:58:45 +0100

    create class TCPClient and implement method run

---
+++ app/src/main/java/com/example/einzelabgabe_se2_zangerl/TCPClient.java
@@ -0,0 +1,33 @@
+package com.example.einzelabgabe_se2_zangerl;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class TCPClient implements Runnable {
+
+    private String message;
+    private String answer;
+
+    public TCPClient(String message){
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Socket socket = new Socket("se2-isys.aau.at", 53212);
+            DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            outToServer.writeBytes(message);
+            this.answer = inFromServer.readLine();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
